823b207f8a586493ce33f17e27001bd067e9a42b
BernardoPavani <bernardo.c.p.marinho@gmail.com>
2024-10-26 15:54:53 -0300

    add Classe D

---
+++ ClasseD.java
@@ -0,0 +1,21 @@
+public class ClasseD {
+    private int D1;
+    private float D2;
+
+    public int getD1() {
+        return D1;
+    }
+
+    public float getD2() {
+        return D2;
+    }
+
+    public void MD1(){
+        System.out.println("Metodo MD1");
+    }
+
+    public void MD2(){
+        System.out.println("Metodo MD2");
+    }
+
+}
